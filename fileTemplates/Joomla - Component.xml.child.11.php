c5ee336e8365fe7a9a61dbc346bb93e571f26b3c
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2022-05-31 14:38:53 +0300

    Updates

---
+++ fileTemplates/Joomla - Component.xml.child.11.php
@@ -0,0 +1,8 @@
+<?php
+#parse("PHP File Header.php")
+
+defined('_JEXEC') or die;
+
+class com_${NAME}InstallerScript {
+
+}
---
+++ fileTemplates/Joomla - View.php.child.1.php
@@ -0,0 +1,5 @@
+<?php
+#parse("PHP File Header.php")
+
+defined('_JEXEC') or die;
+
