f6041f8b20a2c8330c9f401c89f4bc85f63f9763
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2024-06-21 14:24:45 +0300

    JetBrains: PhpStorm v2.0.0 (#1)

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# JetBrains PhpStorm
+# JetBrains: PhpStorm
+
+![JetBrains: PhpStorm](https://repository-images.githubusercontent.com/99415866/b9e8a080-c05e-11e9-80cc-19c2e91b4dc1)
 Various useful files for working in the JetBrains PhpStorm
+
+[![Support](https://img.shields.io/github/issues-raw/SeptdirWorkshop/JetBrains-PhpStorm?style=for-the-badge&logoWidth=20&label=support)](https://github.com/SeptdirWorkshop/JetBrains-PhpStorm/issues)
+[![Download](https://img.shields.io/github/release/SeptdirWorkshop/JetBrains-PhpStorm.svg?style=for-the-badge&colorA=555&colorB=1e87f0&label=download)](https://github.com/SeptdirWorkshop/JetBrains-PhpStorm/releases/latest)
+
+## Package contents
+
+- **codeStyle** - Code formating style
+- **colors** - Code color scheme
+- **fileTemplates** - File templates for Joomla CMS and beyond
+- **options** - File names for templates
+- **project** - Template for an empty project including phing tasks
+- **templates** - Live Templates for files
+- **watchers** - Configuration for File Watcher
--- project/.phing/ProjectTask.php
+++ project/.phing/ProjectTask.php
@@ -1,12 +1,38 @@
 <?php
 
+use Phing\Io\File;
+use Phing\Task;
+use Phing\Type\FileSet;
+
 class ProjectTask extends Task
 {
-	protected $action = null;
-	protected $name = null;
-	protected $version = null;
-	protected $devVersion = null;
-	protected $root = null;
+	protected ?string $root = null;
+	protected ?string $action = null;
+	protected ?string $name = null;
+	protected ?string $version = null;
+	protected ?string $devVersion = null;
+	protected ?string $date = null;
+	protected array $filesExcludes = [
+		'**/.idea/**',
+		'**/.packages/**',
+		'**/.phing/**',
+		'**/node_modules/**',
+		'**/vendor/**',
+		'**/.gitignore',
+		'**/LICENSE',
+		'**/*.md,'
+	];
+	protected array $packageExcludes = [
+		'**/.idea/**',
+		'**/.packages/**',
+		'**/.phing/**',
+		'**/node_modules/**',
+		'**/vendor/**',
+		'**/build/**',
+		'**/.gitignore',
+		'**/LICENSE',
+		'**/*.md,'
+	];
 
 	public function setAction($action)
 	{
@@ -21,10 +47,9 @@ class ProjectTask extends Task
 	public function setVersion($version)
 	{
 		$this->version = $version;
-
-		$version    = explode('.', $version);
-		$count      = count($version);
-		$devVersion = array();
+		$version       = explode('.', $version);
+		$count         = count($version);
+		$devVersion    = [];
 		foreach ($version as $i => $number)
 		{
 			$number       = (int) $number;
@@ -32,6 +57,7 @@ class ProjectTask extends Task
 		}
 
 		$this->devVersion = implode('.', $devVersion);
+		$this->date       = date('F Y');
 	}
 
 	public function setRoot($root)
@@ -46,15 +72,25 @@ class ProjectTask extends Task
 		return $this->$action();
 	}
 
-	protected function info()
+	public function info()
 	{
-		echo '==== Project Info ===' . PHP_EOL
-			. 'Name               ' . $this->name . PHP_EOL
-			. '[RELEASE] Version  ' . $this->release . PHP_EOL
-			. '[RELEASE] Package  ' . $this->getPackageName() . PHP_EOL
-			. '[DEV] Version      ' . $this->devVersion . PHP_EOL
-			. '[DEV] Package      ' . $this->getPackageName(true) . PHP_EOL
-			. 'Base Directory     ' . realpath($this->root) . PHP_EOL;
+		echo implode(PHP_EOL, [
+			'==== Project Info ===',
+			'Name               ' . $this->name,
+			'Date               ' . $this->date,
+			'[RELEASE] Version  ' . $this->version,
+			'[RELEASE] Package  ' . $this->getPackageName(),
+			'[DEV] Version      ' . $this->devVersion,
+			'[DEV] Package      ' . $this->getPackageName(true),
+			'Base Directory     ' . realpath($this->root),
+		]);
+	}
+
+	protected function getPackageName($dev = false)
+	{
+		$version = ($dev) ? $this->devVersion : $this->version;
+
+		return $this->name . '_' . $version . '.zip';
 	}
 
 	protected function prepareRelease()
@@ -65,9 +101,8 @@ class ProjectTask extends Task
 		echo ($files = $this->replaceVersion($this->version)) ? 'OK' : 'ERROR';
 		echo PHP_EOL;
 
-		$date = date('F Y');
 		echo 'Replace date .......... ';
-		echo ($files = $this->replaceDate($date, $files)) ? 'OK' : 'ERROR';
+		echo ($this->replaceDate($files)) ? 'OK' : 'ERROR';
 		echo PHP_EOL;
 	}
 
@@ -85,8 +120,7 @@ class ProjectTask extends Task
 
 		echo 'Find Files ....... ';
 		$root  = $this->root . DIRECTORY_SEPARATOR;
-		$files = (!empty($files)) ? $files
-			: $this->getFiles($root, array('.idea/', '.packages/', '.phing/', 'build/', '.gitignore', 'LICENSE', '*.md'));
+		$files = (!empty($files)) ? $files : $this->getFiles($root, $this->filesExcludes);
 		echo ($files) ? 'OK' : 'ERROR' . PHP_EOL;
 		echo PHP_EOL;
 
@@ -95,7 +129,7 @@ class ProjectTask extends Task
 		{
 			foreach ($files as $path)
 			{
-				$file     = new PhingFile($root, $path);
+				$file     = new File($root, $path);
 				$filename = $file->getAbsolutePath();
 				$original = file_get_contents($filename);
 				$replace  = preg_replace('/@since(\s*)(.?)*/', '@since${1}' . '__DEPLOY_VERSION__', $original);
@@ -106,9 +140,9 @@ class ProjectTask extends Task
 			}
 			echo 'OK';
 		}
-		catch (Exception $exception)
+		catch (Exception $e)
 		{
-			echo 'ERROR';
+			echo 'ERROR: ' . $e->getMessage();
 		}
 		echo PHP_EOL;
 	}
@@ -121,6 +155,11 @@ class ProjectTask extends Task
 		echo ($files = $this->replaceVersion($this->devVersion)) ? 'OK' : 'ERROR';
 		echo PHP_EOL;
 
+		$date = date('F Y');
+		echo 'Replace date .......... ';
+		echo ($files = $this->replaceDate($date, $files)) ? 'OK' : 'ERROR';
+		echo PHP_EOL;
+
 		echo 'Check PhpStorm copyrights ....... ';
 		echo ($files = $this->checkPhpStormCopyrights('__DEPLOY_VERSION__', date('F Y'))) ? 'OK' : 'ERROR';
 		echo PHP_EOL;
@@ -130,41 +169,41 @@ class ProjectTask extends Task
 	{
 		echo '==== Package ' . $this->name . ' ' . $this->devVersion . ' Dev ===' . PHP_EOL;
 		echo 'Create package ....... ';
-		echo ($files = $this->createPackage($this->getPackageName(true))) ? 'OK' : 'ERROR';
+		echo ($this->createPackage($this->getPackageName(true))) ? 'OK' : 'ERROR';
 		echo PHP_EOL;
 	}
 
-	protected function replaceVersion($version = '', $files = array())
+	protected function replaceVersion($version = '', $files = [])
 	{
 		$root  = $this->root . DIRECTORY_SEPARATOR;
-		$files = (!empty($files)) ? $files
-			: $this->getFiles($root, array('.idea/', '.packages/', '.phing/', 'build/node_modules', '.gitignore', 'LICENSE', '*.md'));
-
-		if (empty($files)) return false;
+		$files = (!empty($files)) ? $files : $this->getFiles($root, $this->filesExcludes);
+		if (empty($files))
+		{
+			return false;
+		}
 
 		$dev           = ($version == $this->devVersion);
 		$docVersion    = ($dev) ? '__DEPLOY_VERSION__' : $version;
 		$deployVersion = ($dev) ? '__DEPLOY_VERSION__' : $version;
 		foreach ($files as $path)
 		{
-			$file     = new PhingFile($root, $path);
+			$file     = new File($root, $path);
 			$filename = $file->getAbsolutePath();
 			$original = file_get_contents($filename);
 			$replace  = preg_replace('/@version(\s*)(.?)*/', '@version${1}' . $docVersion, $original);
 			$replace  = preg_replace('/\<version\>(.?)*<\/version\>/', '<version>' . $version . '</version>', $replace);
 			$replace  = preg_replace('/\*Version:(\s*)(.?)*/', '* Version:${1}' . $version, $replace);
 			$replace  = str_replace('__DEPLOY_VERSION__', $deployVersion, $replace);
+			if (strpos($path, '.json') !== false)
+			{
+				$json    = json_decode($replace);
+				$replace = str_replace('"version": "' . $json->version . '",',
+					'"version": "' . $version . '",', $replace);
+			}
 			if ($original != $replace)
 			{
 				file_put_contents($filename, $replace);
 			}
-		}
-
-		$package = $root . '/build/package.json';
-		if ($context = @file_get_contents($package))
-		{
-			$context = preg_replace('/"version": "(.?)*"/', '"version": "' . $version . '"', $context);
-			file_put_contents($package, $context);
 		}
 
 		return $files;
@@ -173,7 +212,7 @@ class ProjectTask extends Task
 	protected function checkPhpStormCopyrights($version = '', $date = '')
 	{
 		$root = $this->root . DIRECTORY_SEPARATOR . '.idea/copyright';
-		if (!$files = $this->getFiles($root, array('profiles_settings.xml'))) return false;
+		if (!$files = $this->getFiles($root, ['profiles_settings.xml'])) return false;
 
 		foreach ($files as $path)
 		{
@@ -191,23 +230,23 @@ class ProjectTask extends Task
 		return true;
 	}
 
-	protected function replaceDate($date = '', $files = array())
+	protected function replaceDate($files = [])
 	{
 		$root  = $this->root . DIRECTORY_SEPARATOR;
 		$files = (!empty($files)) ? $files
-			: $this->getFiles($root, array('.idea/', '.packages/', '.phing/', 'build/', '.gitignore', 'LICENSE', '*.md'));
+			: $this->getFiles($root, ['.idea/', '.packages/', '.phing/', 'node_modules/', '.gitignore', 'LICENSE', '*.md']);
 
 		if (empty($files)) return false;
 
 		foreach ($files as $path)
 		{
-			$file     = new PhingFile($root, $path);
+			$file     = new File($root, $path);
 			$filename = $file->getAbsolutePath();
 			$original = file_get_contents($filename);
-			$replace  = preg_replace('/@date(\s*)(.?)*/', '@date${1}' . $date, $original);
-			$replace  = preg_replace('/\<date\>(.?)*<\/date\>/', '<date>' . $date . '</date>', $replace);
-			$replace  = preg_replace('/\<creationDate\>(.?)*<\/creationDate\>/', '<creationDate>' . $date . '</creationDate>', $replace);
-			$replace  = str_replace('__DEPLOY_DATE__', $date, $replace);
+			$replace  = preg_replace('/@date(\s*)(.?)*/', '@date${1}' . $this->date, $original);
+			$replace  = preg_replace('/\<date\>(.?)*<\/date\>/', '<date>' . $this->date . '</date>', $replace);
+			$replace  = preg_replace('/\<creationDate\>(.?)*<\/creationDate\>/', '<creationDate>' . $this->date . '</creationDate>', $replace);
+			$replace  = str_replace('__DEPLOY_DATE__', $this->date, $replace);
 			if ($original != $replace)
 			{
 				file_put_contents($filename, $replace);
@@ -217,7 +256,7 @@ class ProjectTask extends Task
 		return $files;
 	}
 
-	protected function createPackage($package = '', $files = array())
+	protected function createPackage($package = '', $files = [])
 	{
 		$root      = $this->root . DIRECTORY_SEPARATOR;
 		$directory = $root . '.packages/';
@@ -226,10 +265,12 @@ class ProjectTask extends Task
 			return (unlink($directory . $package)) ? $this->createPackage($package, $files) : false;
 		}
 		$package = $directory . $package;
-		$files   = (!empty($files)) ? $files
-			: $this->getFiles($root, array('.idea/', '.packages/', '.phing/', 'build/', '.gitignore', 'LICENSE', '*.md'));
 
-		if (empty($files)) return false;
+		$files = (!empty($files)) ? $files : $this->getFiles($root, $this->packageExcludes);
+		if (empty($files))
+		{
+			return false;
+		}
 
 		$zip = new ZipArchive();
 		if ($zip->open($package, ZIPARCHIVE::CREATE) !== true)
@@ -239,7 +280,7 @@ class ProjectTask extends Task
 
 		foreach ($files as $path)
 		{
-			$file  = new PhingFile($root, $path);
+			$file  = new File($root, $path);
 			$clear = $file->getPathWithoutBase($root);
 			$clear = str_replace('\\', '/', $clear);
 
@@ -268,12 +309,5 @@ class ProjectTask extends Task
 		$scanner = $fileset->getDirectoryScanner($this->project);
 
 		return $scanner->getIncludedFiles();
-	}
-
-	protected function getPackageName($dev = false)
-	{
-		$version = ($dev) ? $this->devVersion : $this->version;
-
-		return $this->name . '_' . $version . '.zip';
 	}
 }
